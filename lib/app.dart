579d7b681493a54e6746da25523f9d75d2ee435f
Dumenu Derrick <jerome.dumenu@gamil.com>
2025-11-28 10:34:20 +0000

    modified MyApp to App

--- lib/app.dart
+++ lib/app.dart
@@ -3,8 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:pokemon/screens/indexscreen.dart';
 import 'package:pokemon/utils/app_colors.dart';
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -13,19 +13,17 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         fontFamily: 'MyCustomFont',
         appBarTheme: const AppBarTheme(
-          
-         backgroundColor: AppColors.kscaffoldBackgroundColor,
+          backgroundColor: AppColors.kscaffoldBackgroundColor,
           iconTheme: IconThemeData(color: AppColors.kiconthemeColor),
         ),
         scaffoldBackgroundColor: AppColors.kscaffoldBackgroundColor,
       ),
       home: DeviceFrame(
         device: Devices.ios.iPhone13,
-        
+
         isFrameVisible: true,
         orientation: Orientation.portrait,
         screen: const IndexScreen(),
-        
       ),
     );
   }
